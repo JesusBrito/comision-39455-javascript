99ee9db71d07c2a32959ddb4dfec2e4a5a04d52a
jesusbrito <udvuvvdu29>
2023-03-27 16:36:34 -0600

    Ejemplos 12

---
+++ Clase 11 (Repaso)/index.js
@@ -0,0 +1,33 @@
+import {obtenerFecha} from "./utilidades.js"
+
+// function suma(parametroUno, parametroDos) {
+
+//   return;
+// }
+
+// suma(5, 7);
+
+// const restar = function () {};
+
+// restar();
+
+// const multipli = (parametroUno, parametroDos) => parametroUno * parametroDos;
+
+// multipli();
+
+// class Automovil {
+//   constructor(modelo, marca, anio, numeroPuertas) {
+//     this.modelo = modelo;
+//     this.marca = marca;
+//     this.anio = anio;
+//     this.numeroPuertas = numeroPuertas;
+//   }
+// }
+
+// const objetoAutomovil = new Automovil()
+
+
+// const arrayProductos = []
+
+
+console.log(obtenerFecha())
---
+++ Clase 11 (Repaso)/utilidades.js
@@ -0,0 +1,3 @@
+export function obtenerFecha() {
+    return new Date()
+}
